e16bcd0e245dc21aacbc5ccc5a8c6ee0990e6e5f
tobyrogovsky <tobyrogovsky@gmail.com>
2025-06-25 21:20:15 -0400

    made num column

--- 1 tables/1 table.sql
+++ 1 tables/1 table.sql
@@ -1,3 +1,6 @@
 use humanbodydb
 go
-create TABLE dbo.body(ID INT not null identity primary key)
+drop table if exists body
+go
+create TABLE dbo.body(ID INT not null identity primary key,
+num int null)
