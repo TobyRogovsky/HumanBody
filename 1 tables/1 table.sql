d307ec82f87a322a355888282be650885231debb
tobyrogovsky <tobyrogovsky@gmail.com>
2025-06-26 19:43:37 -0400

    1st commit - trial branch

--- 1 tables/1 table.sql
+++ 1 tables/1 table.sql
@@ -1,3 +1,8 @@
 use humanbodydb
 go
-create TABLE dbo.body(ID INT not null identity primary key)
+drop table if exists body
+go
+create TABLE dbo.body(
+ID INT not null identity primary key,
+bodypart varchar
+)
---
+++ 2 data/2 data.sql
@@ -0,0 +1,4 @@
+insert body (ID)
+select 3
+union select 2
+union select 1
